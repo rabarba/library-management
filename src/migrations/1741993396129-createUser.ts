118110bc4fbbb687d51c928ba3ca486d352cd1fc
Ugur Kap <ugurcan.kap@prettylittlething.com>
2025-03-15 02:05:22 +0300

    feat: add user table

---
+++ src/migrations/1741993396129-createUser.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateUser1741993396129 implements MigrationInterface {
+    name = 'CreateUser1741993396129'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "user" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "user"`);
+    }
+
+}
