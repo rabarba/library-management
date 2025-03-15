cf39541ed319d1fa987ba0597238aa2610afa317
Ugur Kap <119315622+ugur-kap@users.noreply.github.com>
2025-03-15 06:15:03 +0300

    Delete dist directory

--- dist/controllers/userController.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getUsers = void 0;
-const getUsers = (req, res) => {
-    res.status(200).json([{ id: 1, name: 'John Doe' }]);
-};
-exports.getUsers = getUsers;
